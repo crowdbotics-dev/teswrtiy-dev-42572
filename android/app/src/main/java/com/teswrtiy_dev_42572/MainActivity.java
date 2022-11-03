b4cf56dc6883c156b345b94847f28e9729cf146a
Crowdbotics <engineering@crowdbotics.com>
2022-11-03 11:19:35 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/teswrtiy_dev_42572/MainActivity.java
@@ -0,0 +1,15 @@
+package com.teswrtiy_dev_42572;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "teswrtiy_dev_42572";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "teswrtiy_dev_42572",
+  url: "https://teswrtiy_dev_42572.botics.co",
+  api: "https://teswrtiy_dev_42572.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
